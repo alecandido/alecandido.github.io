e88fc5b252ada5d622d04a57082803c83c24219c
AleCandido <candido.ale@gmail.com>
2020-06-04 00:22:31 +0200

    Still failing with gulpfile (merge stream and watchify)

---
+++ _ts/main-hello.ts
@@ -0,0 +1,10 @@
+import { sayHello } from "./greet";
+
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    if (elt != null) {
+        elt.innerText = sayHello(name);
+    }
+}
+
+showHello("greeting", "TypeScript");
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,77 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var watchify = require('watchify');
-var tsify = require('tsify');
-var fancy_log = require('fancy-log');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
-var paths = {
-    pages: ['*.html']
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var watchify = require("watchify");
+var tsify = require("tsify");
+var fancy_log = require("fancy-log");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
+var rename = require("gulp-rename");
+var glob = require("glob");
+var es = require("event-stream");
+
+// copy html
+
+var html_paths = {
+  pages: ["*.html"]
 };
 
-
-gulp.task('copy-html', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
+gulp.task("copy-html", function () {
+  return gulp.src(html_paths.pages).pipe(gulp.dest("dist"));
 });
 
+// default
 
-var watchedBrowserify = watchify(browserify({
-      basedir: '.',
+const ts_entries_pattern = "_ts/main-*.ts";
+const ts_entries = glob.sync(ts_entries_pattern);
+
+function task_factory(ts_entry) {
+  var watchedBrowserify = watchify(
+    browserify({
+      basedir: ".",
       debug: true,
-      entries: ['_ts/main.ts', '_ts/ciao.ts'],
+      entries: ts_entry,
       cache: {},
       packageCache: {}
-}).plugin(tsify));
+    }).plugin(tsify)
+  );
 
-function bundle() {
-    return watchedBrowserify
-        .transform('babelify', {
-            presets: ['es2015'],
-            extensions: ['.ts']
+  function bundle() {
+    return (
+      watchedBrowserify
+        .transform("babelify", {
+          presets: ["es2015"],
+          extensions: [".ts"]
         })
         .bundle()
-        .on('error', fancy_log)
-        .pipe(source('bundle.js'))
+        .on("error", fancy_log)
+        //.pipe(source("bundle.js"))
+        .pipe(source(ts_entry))
+        // rename them to have "bundle as postfix"
+        .pipe(
+          rename({
+            extname: ".bundle.js"
+          })
+        )
         .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('assets/js'));
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("assets/js"))
+    );
+  }
+
+  //watchedBrowserify.on("update", bundle);
+  //watchedBrowserify.on("log", fancy_log);
+
+  return bundle;
 }
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
-watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', fancy_log);
+var tasks = ts_entries.map(task_factory);
+function f(tasks) {
+  return es.merge.apply(null, tasks);
+}
+
+//gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
+gulp.task("default", gulp.series(gulp.parallel("copy-html"), f(tasks)));
