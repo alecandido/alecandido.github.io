47eade29f295245622b797d07a120c2fdf1a3f3e
Alessandro Candido <candido.ale@gmail.com>
2020-10-10 18:35:33 +0200

    Reorganize the repo, add about section

---
+++ README.md
@@ -0,0 +1,8 @@
+# Personal WebSite
+
+My personal website. Goals:
+
+- try to make one
+- showcase something as soon as I have time to fill it
+
+Mainly powered by `bulma` and `next.js`.
---
+++ about/pages/about.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>about - {siteTitle}</title>
+      </Head>
+
+      <div>About us</div>
+      <div>
+        Back to{" "}
+        <Link href="/" as={process.env.BACKEND_URL + "/"}>
+          <a>Projects</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
