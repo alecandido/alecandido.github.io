e571fc2c9e0f3ea2f0a16ba2b7401297db18c4f1
Alessandro Candido <candido.ale@gmail.com>
2020-11-19 01:51:55 +0100

    Add menu toggle

---
+++ home/assets/js/home.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * @param {HTMLElement} el
+ * @return {{top: number, left: number}}
+ */
+function getDocumentOffsetPosition(el) {
+  var position = {
+    top: el.offsetTop,
+    left: el.offsetLeft,
+  };
+  if (el.offsetParent) {
+    var parentPosition = getDocumentOffsetPosition(el.offsetParent);
+    position.top += parentPosition.top;
+    position.left += parentPosition.left;
+  }
+  return position;
+}
+
+// open/close launcher menu
+
+var menuBtn = document.querySelector(".menu-toggle-btn");
+var launcherCol = document.querySelector("#launcher-column");
+var launchers = launcherCol.getElementsByClassName("launcher");
+var presentationCol = document.querySelector("#presentation-column");
+
+menuBtn.onclick = function () {
+  menuBtn.classList.toggle("open");
+  launcherCol.classList.toggle("close");
+  Object.values(launchers).map((launcher) => {
+    launcher.classList.toggle("close");
+  });
+  presentationCol.classList.toggle("static-pos");
+};
