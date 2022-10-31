2bc0f6f4a372c7fcd3bd05c682eff5ccd423ed5c
Alessandro Candido <candido.ale@gmail.com>
2022-10-31 17:02:02 +0100

    Remove appshell content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,9 @@
   import '@brainandbones/skeleton/styles/all.css';
   import '../app.postcss';
 
-  import { AppShell, AppBar, GradientHeading } from '@brainandbones/skeleton';
+  import { AppShell } from '@brainandbones/skeleton';
 </script>
 
 <AppShell>
-  <!-- Sidebar -->
-  <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4" />
-
-  <svelte:fragment slot="sidebarLeft">
-    <!-- Insert the list: -->
-    <nav class="list-nav">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
-    </nav>
-    <!-- --- -->
-  </svelte:fragment>
-
-  <!-- Page Content Slot -->
   <slot />
 </AppShell>
