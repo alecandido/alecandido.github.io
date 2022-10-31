e86501680da21e488e149e1e66c1cbdc72ce36c0
Alessandro Candido <candido.ale@gmail.com>
2022-10-31 12:22:57 +0100

    Introduce adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      strict: false
+    }),
+    prerender: {
+      onError: 'continue'
+    }
+  }
 };
 
 export default config;
